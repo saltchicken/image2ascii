10d2d242b30454f28223a722195215c1ac1f4a83
saltchicken <john.eicher89@gmail.com>
2024-09-14 16:25:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# from PIL import Image, ImageFont
+import argparse
+import cv2
+
+ASCII_ORDERS = {
+    "basic": (
+        " ",
+        ".",
+        "'",
+        ",",
+        ":",
+        ";",
+        "c",
+        "l",
+        "x",
+        "o",
+        "k",
+        "X",
+        "d",
+        "O",
+        "0",
+        "K",
+        "N",
+    )
+}
+
+
+def convert_row_to_ascii(row):
+    order = ASCII_ORDERS["basic"]
+    return tuple(order[int(x / (255 / 16))] for x in row)[::-1]
+
+
+def convert_to_ascii(input_grays):
+    return tuple(convert_row_to_ascii(row) for row in input_grays)
+
+
+def convert_image_to_ascii(resolution: int):
+    img = cv2.imread("test.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (resolution, resolution))
+    # cv2.imshow("Image", img)
+
+    converted = convert_to_ascii(img)
+    for row in converted:
+        print(" ".join(row))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Set resolution for the script.")
+    parser.add_argument(
+        "--resolution", type=int, default=128, help="Resolution (default is 128)"
+    )
+
+    args = parser.parse_args()
+    resolution = args.resolution
+
+    convert_image_to_ascii(resolution)
+
+
+if __name__ == "__main__":
+    main()
